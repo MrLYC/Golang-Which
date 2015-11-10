d98f30facce3c2d239ae3c4bc3cc7250d223db7f
MrLYC <imyikong@gmail.com>
2015-11-10 10:55:56 +0800

    fix args parser

--- src/argument.go
+++ src/argument.go
@@ -3,39 +3,17 @@ package main
 
 import (
 	"flag"
-	"fmt"
 )
 
 type Argument struct {
-	filename    string
-	option_all  bool
-	option_help bool
+	filename   string
+	option_all bool
 }
 
 func (this *Argument) ParseArgs() {
-	filename := flag.String("filename", "", "")
-	option_all := flag.Bool(
-		"a", false, "print all matching pathnames of each argument",
-	)
-	option_s_help := flag.Bool(
-		"h", false, "show this help message and exit",
-	)
-	option_help := flag.Bool(
-		"help", false, "show this help message and exit",
+	flag.BoolVar(
+		&(this.option_all), "a", false, "print all matching pathnames of each argument",
 	)
 	flag.Parse()
-
-	this.filename = *filename
-	this.option_all = *option_all
-	this.option_help = *option_s_help || *option_help
-}
-
-func (this *Argument) PrintHelp() {
-	fmt.Println("usage: which - locate a command [-h] [-a] filename\n")
-	fmt.Println("positional arguments:")
-	fmt.Println("  filename")
-	fmt.Println("\n")
-	fmt.Println("optional arguments:")
-	fmt.Println("  -a          print all matching pathnames of each argument")
-	fmt.Println("  -h, --help  show this help message and exit")
+	this.filename = flag.Arg(0)
 }
--- src/which.go
+++ src/which.go
@@ -9,32 +9,21 @@ import (
 func main() {
 	argument := new(Argument)
 	argument.ParseArgs()
-	switch {
-	case argument.option_help:
-		{
-			argument.PrintHelp()
-			syscall.Exit(0)
-		}
-	case len(argument.filename) > 0:
-		{
-			cmd := NewCommand(argument.filename)
-			next := cmd.Find()
-			existed := false
-			for path := next(); len(path) > 0; path = next() {
-				fmt.Println(path)
-				existed = true
-				if !argument.option_all {
-					break
-				}
+	if len(argument.filename) > 0 {
+		cmd := NewCommand(argument.filename)
+		next := cmd.Find()
+		existed := false
+		for path := next(); len(path) > 0; path = next() {
+			fmt.Println(path)
+			existed = true
+			if !argument.option_all {
+				break
 			}
-			if !existed {
-				syscall.Exit(1)
-			}
-			syscall.Exit(0)
 		}
-	default:
-		{
-			syscall.Exit(2)
+		if !existed {
+			syscall.Exit(1)
 		}
+		syscall.Exit(0)
 	}
+	syscall.Exit(2)
 }
